2ddacf46d26322be140d12f3c095a3149a97ab6d
Noor-bot12 <167661909+Noor-bot12@users.noreply.github.com>
2024-05-08 10:55:32 +0600

    Update alluser.js

--- modules/commands/alluser.js
+++ modules/commands/alluser.js
@@ -3,7 +3,7 @@ module.exports.config = {
     version: "1.0.5",
     permission: 0,
     prefix: false,
-    credits: "Deku",
+    credits: "nazrul",
     description: "Get all uid and names in Group.",
     category: "without prefix",
     cooldowns: 2
