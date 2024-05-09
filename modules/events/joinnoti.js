2d7cdeb364b66fb78062b04d2d407caecd565110
SUMI-BOT12 <farifo9573@twnker.com>
2024-05-09 21:44:15 +0600

    Update joinnoti.js

--- modules/events/joinnoti.js
+++ modules/events/joinnoti.js
@@ -13,9 +13,9 @@ module.exports.run = async function({ api, event }) {
   const request = require("request");
   const { threadID } = event;
   if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
-    api.changeNickname(`❤️💛𝗡𝗔𝗭𝗥𝗨𝗟 𝗕𝗢𝗧❤️💛`, threadID, api.getCurrentUserID());
-    return api.sendMessage(`🥀🥀আসসালামু আলাইকুম 🥀🥀\nআসা করি সাবাই আল্লাহ রহমতে ভালো আছেন\nযে কোন কমান্ড দেখতে ${global.config.PREFIX}help ব্যবহার করুন\n╔══════☆♡☆══════╗\n\n╰┈➤উদাহারণ➤\n\n╰┈➤${global.config.PREFIX}inf ➤ \n╰┈➤${global.config.PREFIX}admin ➤ \n╰┈➤${global.config.PREFIX}owner ➤ \n╰┈➤${global.config.PREFIX}help ➤\n\n┗━━━ ✦❘༻༺❘✦ ━━━━┛\n
-★যেকোনো অভিযোগ অথবা হেল্প এর জন্য আমার★\n★বস 𝗡𝗔𝗭𝗥𝗨𝗟 কে নক করতে পারেন★\n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊: https://www.facebook.com/ji.la.pi.6\n𝐌𝐄𝐒𝐒𝐄𝐍𝐆𝐀𝐑𝐄: https://m.me/ji.la.pi.6`, threadID);
+    api.changeNickname(`𝗡𝗔𝗭𝗥𝗨𝗟 𝗕𝗢𝗧`, threadID, api.getCurrentUserID());
+    return api.sendMessage(`🥀🥀আসসালামু আলাইকুম 🥀🥀\nআসা করি সাবাই আল্লাহ রহমতে ভালো আছেন\nযে কোন কমান্ড দেখতে ${global.config.PREFIX}help ব্যবহার করুন\n╔══════☆♡☆══════╗\n╰┈➤উদাহারণ\n\n╰┈➤${global.config.PREFIX}inf\n╰┈➤${global.config.PREFIX}admin \n╰┈➤${global.config.PREFIX}owner  \n╰┈➤${global.config.PREFIX}help \n┗━━━ ✦❘༻༺❘✦ ━━━━┛\n
+★যেকোনো অভিযোগ অথবা হেল্প এর জন্য আমার\nবস 𝗡𝗔𝗭𝗥𝗨𝗟 কে নক করতে পারেন\n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊: https://www.facebook.com/ji.la.pi.6\n𝐌𝐄𝐒𝐒𝐄𝐍𝐆𝐀𝐑𝐄: https://m.me/ji.la.pi.6`, threadID);
   }
   else {
     try {
@@ -41,21 +41,18 @@ api.getUserInfo(parseInt(userID), (err, data) => {
         memLength.push(participantIDs.length - i++);
 memLength.sort((a, b) => a - b);
  
-      (typeof threadData.customJoin == "undefined") ? msg = "╔════•|      ✿      |•════╗\n আ্ঁস্ঁসা্ঁলা্ঁমু্ঁ💚আ্ঁলা্ঁই্ঁকু্ঁম্ঁ\n╚════•|      ✿      |•════╝\n\n  {name}\n༄✺আ্ঁপ্ঁনা্ঁকে্ঁ আ্ঁমা্ঁদে্ঁর্ঁ✺\n{threadName}\nএ্ঁর্ঁ প্ঁক্ষ্ঁ থে্ঁকে্ঁ\nভা্ঁলো্ঁবা্ঁসা্ঁ অ্ঁভি্ঁরা্ঁম্ঁ\n✺আঁপঁনিঁ এঁইঁ গ্রুঁপেঁর {soThanhVien} নঁং মে্ঁম্বা্ঁরঁ\nন্ঁজ্ঁরু্ঁল্ঁ গ্রুঁপেঁরঁ পঁক্ষঁ থেঁকেঁ আঁপঁনাঁকেঁ স্বাঁগঁতঁমঁ♥\n🥰🥀ᏔᎬᏞᏟϴᎷᎬ 🥀🥰 \n   ┌────♣─────┐\n   😘♦ -জিঁলাঁপিঁ-  ♦😘\n   └────♣─────┘\n \n✢━━━━━━━━━━━━━━━✢\n ----❖----- 𝐍𝐀𝐙𝐑𝐔𝐋 -----❖----" : msg = threadData.customJoin;
+      (typeof threadData.customJoin == "undefined") ? msg = "╔════•|      ✿      |•════╗\n আ্ঁস্ঁসা্ঁলা্ঁমু্ঁ💚আ্ঁলা্ঁই্ঁকু্ঁম্ঁ\n╚════•|      ✿      |•════╝\n\n  {name} \n༄✺আ্ঁপ্ঁনা্ঁকে্ঁ আ্ঁমা্ঁদে্ঁর্ঁ✺\n{threadName}\nএ্ঁর্ঁ প্ঁক্ষ্ঁ থে্ঁকে্ঁ ভা্ঁলো্ঁবা্ঁসা্ঁ অ্ঁভি্ঁরা্ঁম্ঁ\n\n✺আঁপঁনিঁ এঁইঁ গ্রুঁপেঁর {soThanhVien} নঁং মে্ঁম্বা্ঁরঁ\nন্ঁজ্ঁরু্ঁল্ঁ ব্ঁট্ঁ পঁক্ষঁ থেঁকেঁ আঁপঁনাঁকেঁ স্বাঁগঁতঁমঁ♥\n🥰🥀ᏔᎬᏞᏟϴᎷᎬ 🥀🥰 \n    ┌────♣─────┐\n    😘♦ -জিঁলাঁপিঁ-  ♦😘\n    └────♣─────┘\n✢━━━━━━━━━━━━━━━✢\n ----❖----- 𝐍𝐀𝐙𝐑𝐔𝐋 -----❖----" : msg = threadData.customJoin;
       msg = msg
-      .replace(/\{uName}/g, nameArray.join(', '))
+      .replace(/\{name}/g, nameArray.join(', '))
       .replace(/\{type}/g, (memLength.length > 1) ?  'you' : 'Friend')
       .replace(/\{soThanhVien}/g, memLength.join(', '))
       .replace(/\{threadName}/g, threadName);			
  
       var link = [
-"https://i.imgur.com/i6bqJPo.mp4",
-"https://i.imgur.com/i6bqJPo.mp4",
-"https://i.imgur.com/i6bqJPo.mp4",
-"https://i.imgur.com/i6bqJPo.mp4",
+"https://i.imgur.com/7yZ58P8.jpeg",
       ];
-        var callback = () => api.sendMessage({ body: msg, attachment: fs.createReadStream(__dirname + "/cache/leiamnashJ.mp4"), mentions }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/leiamnashJ.mp4"));
-    return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/leiamnashJ.mp4")).on("close", () => callback());       
+        var callback = () => api.sendMessage({ body: msg, attachment: fs.createReadStream(__dirname + "/cache/wellcome.jpg"), mentions }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/wellcome.jpg"));
+    return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/wellcome.jpg")).on("close", () => callback());       
             }
 })
         }
