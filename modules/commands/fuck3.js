5f0c782fc823b7e074f558a48271908077d1cb36
Noor-bot122 <mayaakter7162@gmail.com>
2024-05-11 18:54:54 +0600

    Update fuck3.js

--- modules/commands/fuck3.js
+++ modules/commands/fuck3.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "fuck3",
     version: "3.1.1",
     hasPermssion: 2,
-    credits: "Ayan",
+    credits: "nazrul",
     description: "Get fuck",
     commandCategory: "nsfw",
     usages: "[@mention]",
