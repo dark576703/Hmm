ddcf2281decc458c0d3ec680149fec07b720796a
Noor-bot122 <mayaakter7162@gmail.com>
2024-05-11 20:32:12 +0600

    Update password.js

--- modules/commands/password.js
+++ modules/commands/password.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "password",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "MR CHAND",
+  credits: "NAZRUL",
   description: "",
   commandCategory: "GAME",
   usages: "[text 1] | [text 2]",
