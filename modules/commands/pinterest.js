78735247b1e695657d2b3ee3fb51ba46666b65ea
Noor-bot122 <mayaakter7162@gmail.com>
2024-05-11 20:31:29 +0600

    Update pinterest.js

--- modules/commands/pinterest.js
+++ modules/commands/pinterest.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "pic",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "AYAN CHOWDHURY",
+    credits: "NAZRUL",
     description: "Image search",
     commandCategory: "Search",
     usages: "[Text] - [number]",
