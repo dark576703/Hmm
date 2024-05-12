240bf0f89a5432bf3a55c61fa555ba04442d7657
Noor-bot122 <mayaakter7162@gmail.com>
2024-05-12 17:14:26 +0600

    Update toilet.js

--- modules/commands/toilet.js
+++ modules/commands/toilet.js
@@ -1,18 +1,18 @@
 module.exports.config = {
-	name: "toilet",
-	version: "1.0.1",
-	hasPermssion: 0,
-	credits: "Nazrul",
-	description: "Toilet 🚽",
-	commandCategory: "Image",
-	usages: "rank",
-	cooldowns: 5,
-	dependencies: {
-	  "fs-extra": "",
-	  "axios": "",
-	  "canvas" :"",
-	  "jimp": ""
-	}
+    name: "toilet",
+    version: "1.0.1",
+    hasPermssion: 0,
+    credits: "nazrul",
+    description: "Toilet 🚽",
+    commandCategory: "Image",
+    usages: "rank",
+    cooldowns: 5,
+    dependencies: {
+      "fs-extra": "",
+      "axios": "",
+      "canvas" :"",
+      "jimp": ""
+  }
 };
 
 module.exports.onLoad = async() => {
@@ -22,7 +22,7 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/`;
     const path = resolve(__dirname, 'cache', 'toilet.png');
     if (!existsSync(dirMaterial + "")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/BtSlsSS.jpg", path);
+    if (!existsSync(path)) await downloadFile("https://i.imgur.com/Kn7KpAr.jpg", path);
 
 }
 
@@ -37,23 +37,23 @@ async function makeImage({ one, two }) {
     let pathImg = __root + `/toilet_${one}_${two}.png`;
     let avatarOne = __root + `/avt_${one}.png`;
     let avatarTwo = __root + `/avt_${two}.png`;
-    
+
     let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
     fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
-    
+
     let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
     fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
-    
+
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    hon_img.resize(320, 345).composite(circleOne.resize(70, 70), 100, 200).composite(circleTwo.resize(70, 70), 100, 200);
-    
+    hon_img.resize(292, 345).composite(circleOne.resize(70, 70), 100, 200).composite(circleTwo.resize(70, 70), 100, 200);
+
     let raw = await hon_img.getBufferAsync("image/png");
-    
+
     fs.writeFileSync(pathImg, raw);
     fs.unlinkSync(avatarOne);
     fs.unlinkSync(avatarTwo);
-    
+
     return pathImg;
 }
 async function circle(image) {
@@ -71,9 +71,9 @@ module.exports.run = async function ({ event, api, args, Currencies }) {
     const mention = Object.keys(event.mentions);
     var one = senderID, two = mention[0];
   await Currencies.increaseMoney(event.senderID, parseInt(hc*rd));
-  
+
   if (!two) return api.sendMessage("Please tag 1 person", threadID, messageID);
   else {
-        return makeImage({ one, two }).then(path => api.sendMessage({ body: `বেশি বাল পাকলামির জন্য তোরে টয়লেটে ফেলে দিলাম। এবাল লে ঠেলা বুঝ গু খা বলদ 😁😁`, attachment: fs.createReadStream(path)}, threadID, () => fs.unlinkSync(path), messageID));
+        return makeImage({ one, two }).then(path => api.sendMessage({ body: `বেশি বাল পাকলামির জন্য তোরে টয়লেটে ফেলে দিলাম। এবার লে ঠেলা বুঝ\nগু খা😁😁🤣🤣`, attachment: fs.createReadStream(path)}, threadID, () => fs.unlinkSync(path), messageID));
   }
-      }
+}
